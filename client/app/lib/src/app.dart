b62096d15678a9c331570d5ecd9de5ae4f01d942
Nicholas Apanian <nicholas.apanian@outlook.com>
2021-02-06 17:54:56 -0500

    start firebase integration on client side for notifications

--- client/app/lib/src/app.dart
+++ client/app/lib/src/app.dart
@@ -1,11 +1,17 @@
 import 'package:app/src/config/theme_data.dart';
 import 'package:app/src/routes/index.dart';
 import 'package:app/src/utils/app_state_notifier.dart';
+import 'package:app/src/utils/firebase.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
+  static final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
   Widget build(BuildContext context) {
+    final pushNotificationService = PushNotificationService(_firebaseMessaging);
+    pushNotificationService.initialise();
     return Consumer<AppStateNotifier>(builder: (context, appState, child) {
       return MaterialApp(
         title: 'News!',
