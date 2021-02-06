604f1ef52a1b8b42378b8d36c136a53af907b264
nguyenquannnn <nguyenquan233@gmail.com>
2021-02-06 14:15:21 -0500

    Update text fridge

--- client/app/lib/src/screens/fridge/fridge.dart
+++ client/app/lib/src/screens/fridge/fridge.dart
@@ -144,20 +144,23 @@ class _FridgeState extends State<Fridge> with SingleTickerProviderStateMixin {
       backgroundColor: Color(0xff00DCA7),
       body: Stack(children: [
         Align(
-            alignment: Alignment.centerLeft,
-            child: Text.rich(
-              TextSpan(
-                text: 'Hello', // default text style
-                children: <TextSpan>[
+            alignment: Alignment.topLeft,
+            child: Container(
+                padding: const EdgeInsets.all(40.0),
+                child: Text.rich(
                   TextSpan(
-                      text: ' beautiful ',
-                      style: TextStyle(fontStyle: FontStyle.italic)),
-                  TextSpan(
-                      text: 'world',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                ],
-              ),
-            )),
+                    text: 'Hello', // default text style
+                    style: TextStyle(fontSize: 20),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: ' beautiful ',
+                          style: TextStyle(fontStyle: FontStyle.italic)),
+                      TextSpan(
+                          text: 'world',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ))),
         Transform.scale(
           alignment: Alignment.centerLeft,
           scale: 0.9,
