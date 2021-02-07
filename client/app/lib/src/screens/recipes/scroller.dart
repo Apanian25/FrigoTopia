5b0d0a400305168cd4d9e1b61ac33848c1ae2d97
Nicholas Apanian <nicholas.apanian@outlook.com>
2021-02-07 05:32:48 -0500

    broken code push

---
+++ client/app/lib/src/screens/recipes/scroller.dart
@@ -0,0 +1,29 @@
+import 'package:app/src/app.dart';
+import 'package:app/src/screens/recipes/recipeItem.dart';
+import 'package:flutter/material.dart';
+
+class Scroller extends StatelessWidget {
+  List<Object> items;
+  Function updateQty;
+  Function delete;
+
+  Scroller(items) {
+    this.items = items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // List<String> floufs = new List();
+
+    return ListView.builder(
+        itemCount: this.items.length,
+        itemBuilder: (BuildContext context, int index) {
+          dynamic item = this.items[index];
+          return Container(
+            constraints: BoxConstraints.tightFor(height: 150),
+            child: RecipeItem(
+                item['url'], item['title'], item['img'], item['src']),
+          );
+        });
+  }
+}
