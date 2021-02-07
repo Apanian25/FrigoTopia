e2c0cb088225a9a55f10c82d55c923c528c2de79
nguyenquannnn <nguyenquan233@gmail.com>
2021-02-06 20:42:48 -0500

    Fix firebase notification

--- client/app/lib/main.dart
+++ client/app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:app/src/app.dart';
 import 'package:app/src/utils/app_state_notifier.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -9,11 +11,26 @@ import 'package:provider/provider.dart';
 // import 'package:firebase_messaging/firebase_messaging.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
-  runApp(
-    ChangeNotifierProvider<AppStateNotifier>(
-      create: (_) => AppStateNotifier(),
-      child: App(),
-    ),
-  );
+// void main() {
+//     Future<void> main() async {
+//     WidgetsFlutterBinding.ensureInitialized();
+//     await FirebaseApp.();
+//     runApp(App());
+// }
+// runApp(
+
+//   ChangeNotifierProvider<AppStateNotifier>(
+//     create: (_) => AppStateNotifier(),
+//     child: App(),
+//   ),
+// );
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ChangeNotifierProvider<AppStateNotifier>(
+    create: (_) => AppStateNotifier(),
+    child: App(),
+  ));
+// );?
 }
