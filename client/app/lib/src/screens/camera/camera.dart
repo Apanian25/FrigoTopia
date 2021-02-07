5cf3c138c73738dd655b5fe098e955c2ba50d349
Jonathan Pesce <jonathanpesce@hotmail.com>
2021-02-06 23:29:29 -0500

    Add the select image from gallery

--- client/app/lib/src/screens/camera/camera.dart
+++ client/app/lib/src/screens/camera/camera.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:dio/dio.dart';
 import 'dart:convert';
@@ -34,6 +35,18 @@ class _MyCameraState extends State<CameraState> {
     });
   }
 
+  Future selectImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   Future sendImage() async {
     // http.post('http://127.0.0.1:5000/api/v1/image_upload',
     //     body: {"image": _image});
@@ -56,6 +69,36 @@ class _MyCameraState extends State<CameraState> {
     });
   }
 
+  SpeedDial buildSpeedDial() {
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      animatedIconTheme: IconThemeData(size: 22.0),
+      // child: Icon(Icons.add),
+      onOpen: () => print('OPENING DIAL'),
+      onClose: () => print('DIAL CLOSED'),
+      visible: true,
+      curve: Curves.bounceIn,
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.add_a_photo, color: Colors.white),
+          backgroundColor: Color(0xff00BFA6),
+          onTap: getImage,
+          label: 'Take a picture',
+          labelStyle: TextStyle(fontWeight: FontWeight.w500),
+          labelBackgroundColor: Colors.green[50],
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.file_upload, color: Colors.white),
+          backgroundColor: Color(0xff00BFA6),
+          onTap: selectImage,
+          label: 'Add a picture',
+          labelStyle: TextStyle(fontWeight: FontWeight.w500),
+          labelBackgroundColor: Colors.green[50],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,12 +107,17 @@ class _MyCameraState extends State<CameraState> {
       ),
       body: Center(
         child: _image == null ? Text('No image selected.') : Image.file(_image),
-        //child: Column(children: )
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: _image == null ? getImage : sendImage,
-          tooltip: 'Pick Image',
-          child: _image == null ? Icon(Icons.add_a_photo) : Icon(Icons.send)),
+      // floatingActionButton: FloatingActionButton(
+      //     onPressed: _image == null ? getImage : sendImage,
+      //     tooltip: 'Pick Image',
+      //     child: _image == null ? Icon(Icons.add_a_photo) : Icon(Icons.send)),
+      floatingActionButton: _image == null
+          ? buildSpeedDial()
+          : FloatingActionButton(
+              onPressed: sendImage,
+              tooltip: 'Pick Image',
+              child: Icon(Icons.send)),
     );
   }
 }
